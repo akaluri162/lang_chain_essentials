93f21a9d15b105baaa14a00c29f5b2af4ae26888
akaluri162 <akaluri369@gmail.com>
2023-12-13 23:50:19 -0500

    first_prompt

--- lib/langchain_1.py
+++ lib/langchain_1.py
@@ -4,10 +4,24 @@ import openai
 
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 load_dotenv()  # This loads the environment variables from the .env file
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 llm_model = "gpt-3.5-turbo"
 
+chat = ChatOpenAI(temperature=0.0, model=llm_model)
 
+template_string = """Tell me a fun fact about \
+the country: \
+{country}.
+"""
+
+prompt_template = ChatPromptTemplate.from_template(template_string)
+
+country = "India"
+prompt_country = prompt_template.format_messages(country = country)
+
+gpt_response = chat(prompt_country)
+print(gpt_response.content)
